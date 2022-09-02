bdf177bd273e05416de350bf3b4feb9be6878d52
josueleto77 <94468870+josueleto77@users.noreply.github.com>
2022-09-02 13:41:06 -0400

    Add files via upload

---
+++ sendemail.php
@@ -0,0 +1,42 @@
+<?php
+
+// Define some constants
+define( "RECIPIENT_NAME", "newwebsite" );
+//define( "RECIPIENT_EMAIL", "manzoorhussain84074@gmail.com" );
+define( "RECIPIENT_EMAIL", "info@nxispwr.com" );
+
+// Read the form values
+$success = false;
+$name = isset( $_POST['name'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['name'] ) : "";
+$senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
+$phone = isset( $_POST['phone'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
+$address = isset( $_POST['address'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['address'] ) : "";
+$services = isset( $_POST['services'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['services'] ) : "";
+$bills = isset( $_POST['bills'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['bills'] ) : "";
+$subject = isset( $_POST['subject'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['subject'] ) : "";
+$website = isset( $_POST['website'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['website'] ) : "";
+$message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
+
+$mail_subject = 'A contact request send by ' . $name;
+
+$body = 'Name: '. $name . "\r\n";
+$body .= 'Email: '. $senderEmail . "\r\n";
+
+
+if ($phone) {$body .= 'Phone: '. $phone . "\r\n"; }
+if ($services) {$body .= 'services: '. $services . "\r\n"; }
+if ($bills) {$body .= 'bills: '. $bills . "\r\n"; }
+if ($subject) {$body .= 'Subject: '. $subject . "\r\n"; }
+if ($address) {$body .= 'Address: '. $address . "\r\n"; }
+if ($website) {$body .= 'Website: '. $website . "\r\n"; }
+
+//$body .= 'message: ' . "\r\n" . $message;
+
+
+
+// If all values exist, send the email
+// echo $name."<br/>".$senderEmail."<br/>".$phone."<br/>".$address."<br/>".$services."<br/>".$bills;
+
+
+
+?>
